ccb711e905fe968f562fd951356862b904971048
kibeert <ckibertkoech@gmail.com>
2024-03-05 16:58:08 +0300

    Update signup view and template. (generated by blackbox.ai )

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 def signupview(request, *args, **kwargs ):
-    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #log the user in
+            return redirect("{% url 'articles' %}")
+    else:
+        form = UserCreationForm()
     return render(request, 'accounts/signup.html', {'form' : form})
