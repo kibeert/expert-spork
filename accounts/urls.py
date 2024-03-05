c5e23e13c4bb133329bb491d8691e4017eb9da1a
kibeert <ckibertkoech@gmail.com>
2024-03-05 12:32:43 +0300

    usercreationform

--- accounts/urls.py
+++ accounts/urls.py
@@ -15,18 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from .import views
-
-from django.conf.urls.static import static
-from django.conf import settings
+app_name = 'accounts'
 
 urlpatterns = [
-    path('accounts/', include('accounts.urls')),
-    path('', include('base.urls')),
-    path('', views.homepage, name='home'),
-    path('admin/', admin.site.urls),
+    path('signup/', views.signupview, name='signup')
 ]
-
-
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- nautic/settings.py
+++ nautic/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     #custom apps
     'base',
+    'accounts',
 ]
 
 MIDDLEWARE = [
