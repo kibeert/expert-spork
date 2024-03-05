4c66a341e0578fbe12ca817e12db7f4afeaed1fd
kibeert <ckibertkoech@gmail.com>
2024-03-05 21:44:43 +0300

    Added login view and form to accounts app.

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 # Create your views here.
 def signupview(request, *args, **kwargs ):
     if request.method == "POST":
@@ -13,4 +14,11 @@ def signupview(request, *args, **kwargs ):
     return render(request, 'accounts/signup.html', {'form' : form})
 
 def loginview(request, *args, **kwargs):
-    pass
+    if request.method =="POST":
+        form = AuthenticationForm(data = request.POST)
+        if form.is_valid():
+            return redirect("articles")  
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'accounts/login.html', {'form': form})
